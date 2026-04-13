59ad3f76c86422be23d5ae7d141cc0c682634f0b
Tymur Chernykov <127988729+stealhead1@users.noreply.github.com>
2026-04-13 23:51:02 +0000

    Today, I first created the website elements, using a lot of containers in order to get elements to appear next to each other, and learned different ways to use display: flex. For javascript, I learned how to create elements and add properties to them, which i used in loops to create the grid the game is played in.

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 // add javascript here
+let fieldWidth = 8;
+let fieldLength = 8;
+const snakePos = [];
+
+//gets cell by position to avoid typing long pieces of code
+function getCell(x,y){
+    return document.querySelector('div.cell[position="[' + String(x) + ',' + String(y) + ']"]');
+}
+function createField() {
+    //generates rows and makes them children of field
+    for (let i = 1; i <= fieldWidth; i++) {
+        const row = document.createElement("div");
+        row.id = "row" + String(i);
+        row.classList.add("rows");
+        document.getElementById("field").appendChild(row);
+        //generates cells and makes them children of rows
+        for (let j = 1; j <= fieldLength; j++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.id = "cell" + String((i-1) * fieldLength + j);
+            row.appendChild(cell);
+            //coordinates of the cell are stored as (x,y)
+            cell.setAttribute("position", "[" + String(j) + "," + String(i) + "]");
+        }
+    }
+}
+function startGame(){
+}
+//generates the game field when the page is loaded
+createField();
+document.getElementById("start").addEventListener("click", startGame);
