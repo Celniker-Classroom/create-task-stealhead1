ecd8215ffb5eaa34998c9e627daa90d56586282f
Tymur Chernykov <127988729+stealhead1@users.noreply.github.com>
2026-04-14 20:48:19 +0000

    Today I did a lot of programming. First I made the moveSnake(direction) function, which gets the last key pressed and moves it in a direction in accordance to that. I realized invalid inputs would have to be checked, so the chooseDirection function checks that, and is defaulted to the last direction if the input is invalid. Then, by using that to calculate the next direction, I use const appleEaten = checkAhead(nextHead) in order to check if the ahead block is a apple, snake or wall. I ran into a few syntax errors when getCell returns null when checking for a wall and snake at the same time. Then if an apple is eaten, the length is increased, and updateField function is able to handle it since nothing will be removed. Lastely, the gameOver function re-enables the play button, clears the field, then breaks out of the moveSnake function to prevent the field from being updated again, which is a logic error that happened during developement

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ const snakePos = [];
 const applePos = [];
 let snakeLength;
 let currentDirection = 'd';
-
+let lastDirection = 'd';
+let appleRemoved = false;
+var gameRunning;
+var gameOverCheck = false;
 //gets cell by position to avoid typing long pieces of code
 function getCell(x, y) {
     return document.querySelector('div.cell[position="[' + String(x) + ',' + String(y) + ']"]');
@@ -28,7 +31,36 @@ function createField() {
         }
     }
 }
-
+function gameOver(){
+    clearInterval(gameRunning);
+    for (let k=0; k<snakeLength; k++){
+        getCell(snakePos[k][0], snakePos[k][1]).style.backgroundColor = 'white';
+    }
+    getCell(applePos[0], applePos[1]).style.backgroundColor = 'white';
+    document.getElementById("start").disabled = false;
+    gameOverCheck = true;
+}
+function checkAhead(ahead) {
+    if (ahead[0] < 1 || ahead[0] > fieldLength || ahead[1] < 1 || ahead[1] > fieldWidth) {
+        gameOver();
+        return false;
+    }
+    if (getCell(ahead[0], ahead[1]).style.backgroundColor == 'red') {
+        let validPosition = false;
+        while (!validPosition) {
+            applePos[0] = Math.floor(Math.random() * fieldLength) + 1;
+            applePos[1] = Math.floor(Math.random() * fieldWidth) + 1;
+            if (getCell(applePos[0], applePos[1]).style.backgroundColor != 'green') {
+                validPosition = true;
+            }
+        }
+        return true;
+    }
+    else if (getCell(ahead[0], ahead[1]).style.backgroundColor == 'green') {
+        gameOver();
+    }
+    return false;
+}
 function updateField(removedSnake) {
     if (removedSnake) {
         getCell(removedSnake[0], removedSnake[1]).style.backgroundColor = 'white';
@@ -38,38 +70,64 @@ function updateField(removedSnake) {
     }
     getCell(applePos[0], applePos[1]).style.backgroundColor = 'red';
 }
+function chooseDirection(input) {
+    var move;
+    if ((input == 'w' || input == 'ArrowUp') && (lastDirection != 's' && lastDirection != 'ArrowDown')) {
+        lastDirection = input;
+        move = [0, -1];
+    }
+    else if ((input == 'd' || input == 'ArrowRight') && (lastDirection != 'a' && lastDirection != 'ArrowLeft')) {
+        lastDirection = input;
+        move = [1, 0];
+    }
+    else if ((input == 's' || input == 'ArrowDown') && (lastDirection != 'w' && lastDirection != 'ArrowUp')) {
+        lastDirection = input;
+        move = [0, 1];
+    }
+    else if ((input == 'a' || input == 'ArrowLeft') && (lastDirection != 'd' && lastDirection != 'ArrowRight')) {
+        lastDirection = input;
+        move = [-1, 0];
+    }
+    else {
+        return true;
+    }
+    return move;
+}
 function moveSnake(direction) {
-    let newX = snakePos[snakeLength - 1][0];
-    let newY = snakePos[snakeLength - 1][1];
-    if (direction == 'w' || direction == 'arrowUp') {
-        newY -= 1;
+    var move = chooseDirection(direction);
+    if (move === true) {
+        move = chooseDirection(lastDirection);
     }
-    else if (direction == 'd' || direction == 'arrowRight') {
-        newX += 1;
+    const currentHead = snakePos[snakeLength - 1];
+    const nextHead = [currentHead[0] + move[0], currentHead[1] + move[1]];
+    const appleEaten = checkAhead(nextHead);
+    if (gameOverCheck){
+        return;
     }
-    else if (direction == 's' || direction == 'arrowDown') {
-        newY += 1;
+    snakePos[snakeLength] = nextHead;
+    var removed = null;
+    if (!appleEaten) {
+        removed = snakePos[0];
+        snakePos.splice(0, 1);
+    } else {
+        snakeLength++;
     }
-    else if (direction == 'a' || direction == 'arrowLeft') {
-        newX -= 1;
-    }
-    snakePos[snakeLength] = [newX, newY];
-    var removed = snakePos[0];
-    snakePos.splice(0, 1);
     updateField(removed);
 }
 function startGame() {
     snakeLength = 5;
+    gameOverCheck = false;
     for (i = 0; i < snakeLength; i++) {
         snakePos[i] = [2, 4];
     }
     applePos[0] = 8;
     applePos[1] = 4;
     updateField();
-    setInterval(function () { moveSnake(currentDirection) }, 1000);
+    document.getElementById("start").disabled = true;
+    gameRunning = setInterval(function () { moveSnake(currentDirection) }, 100);
 }
 
 //generates the game field when the page is loaded
 createField();
 document.getElementById("start").addEventListener("click", startGame);
-window.addEventListener('keydown', function (event) { currentDirection = event.key })
+window.addEventListener('keydown', function (event) { currentDirection = event.key; });
